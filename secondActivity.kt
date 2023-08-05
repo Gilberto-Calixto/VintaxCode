b97a0312c2c2f251bd0639beffe50be8dde84039
AloneBoy <gilbertocalixto2002@gmail.com>
2023-08-05 12:01:51 -0300

    version 2 activity second

--- secondActivity.kt
+++ secondActivity.kt
@@ -1,9 +1,11 @@
 package com.example.vintax
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -16,35 +18,54 @@ class secondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
+        val mainItems = mutableListOf<MainItem>()
+        mainItems.add(
+            MainItem(
+                id = 1,
+                drawableId = R.drawable.tomioka,
+                textStringId = R.string.titlenam,
+                color = Color.GREEN
+            )
+        )
+        mainItems.add(
+            MainItem(
+                id = 2,
+                drawableId = R.drawable.ic_launcher_background,
+                textStringId = R.string.geradir,
+                color = Color.CYAN
+            )
+        )
         rcyv = findViewById(R.id.rcyV)
-        val adapter = MainAdapter()
+        val adapter = MainAdapter(mainItems)
         rcyv.adapter = adapter
         rcyv.layoutManager = LinearLayoutManager(this)
     }
 
-    private inner class MainAdapter : RecyclerView.Adapter<MainViewHolder>() {
+    private inner class MainAdapter(private val mainItems : List<MainItem>) : RecyclerView.Adapter<MainViewHolder>() {
 
         //O layout da célula específica (item)
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
             val view = layoutInflater.inflate(R.layout.item_icon, parent, false)
-            val viewHolder = MainViewHolder(view)
-            return viewHolder
-
+            return MainViewHolder(view)
         }
 
         //Quantas céulas essa listagem terá
         override fun getItemCount(): Int {
-            return 20
+            return mainItems.size
         }
 
         //Dispara toda vez que houver uma rolalgem na tela se for preciso trocar o conteúdo
         override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
-            //
+            val itemCurrent = mainItems[position]
+            holder.bind(itemCurrent)
         }
 
     }
 
-    private class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
+    private class MainViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview) {
+        fun bind(item: MainItem){
+            val buttonTest : Button = itemView.findViewById(R.id.buttGrid1)
+            buttonTest.setText(item.textStringId)
+        }
     }
 }
